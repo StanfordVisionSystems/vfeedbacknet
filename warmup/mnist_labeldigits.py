98f93da4655548b0443cdea334a1d7db53c3a48b
jremmons <emmons.john@gmail.com>
2017-05-02 22:54:21 +0000

    renamed the warm up exercises

---
+++ warmup/mnist_labeldigits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+
+def main():
+    batch_size = 32
+    num_classes = 10
+    epochs = 200
+
+    model = Sequential()
+    # describe model below
+
+
+    # load the data
+
+    # begin training
+
+    # store the trained model
+
+if(__name__ == '__main__'):
+    main()
