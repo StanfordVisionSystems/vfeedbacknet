7ca10316962d8d1d6b07b1c923168d62d7c96cb6
jremmons <emmons.john@gmail.com>
2017-05-04 01:06:29 +0000

    finished the warmup exercise

--- warmup/mnist_labeldigits.py
+++ warmup/mnist_labeldigits.py
@@ -6,6 +6,8 @@ from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.utils import plot_model
 
+import numpy as np
+
 def main():
     batch_size = 32
     num_classes = 10
@@ -23,7 +25,7 @@ def main():
     model.add(Flatten())    
     model.add(Dense(128, activation='relu'))
     #model.add(Dropout(0.5))
-    model.add(Dense(num_classes, activation='softmax'))
+    model.add(Dense(10, activation='softmax'))
     
     model.compile(loss=keras.losses.categorical_crossentropy,
                   optimizer=keras.optimizers.Adadelta(),
@@ -35,9 +37,39 @@ def main():
     import mnist_loaddata
     images, labels = mnist_loaddata.read_mnist('/root/mnist/train-images-idx3-ubyte', '/root/mnist/train-labels-idx1-ubyte')
 
-    # begin training
+    images = images.reshape(images.shape[-1], 28, 28, 1)
+    images = images.astype(np.float32)
+    images /= 255
+    one_hot_labels = keras.utils.to_categorical(labels, 10)
 
-    # store the trained model
+    #print images.shape
+    #print one_hot_labels.shape
+    #print labels[0]
+    #print one_hot_labels[0]
+
+    # begin training
+    model.fit(images, one_hot_labels,
+              batch_size=32,
+              epochs=1,
+              verbose=1,
+              validation_data=(images, one_hot_labels)
+    )
+    
+    
+    # store the trained model    
+    
+
+    # get the accruacy on the test set
+    images, labels = mnist_loaddata.read_mnist('/root/mnist/t10k-images-idx3-ubyte', '/root/mnist/t10k-labels-idx1-ubyte')
+        
+    images = images.reshape(images.shape[-1], 28, 28, 1)
+    images = images.astype(np.float32)
+    images /= 255
+    one_hot_labels = keras.utils.to_categorical(labels, 10)
+
+    score = model.evaluate(images, one_hot_labels, verbose=1)
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
 
 if(__name__ == '__main__'):
     main()
