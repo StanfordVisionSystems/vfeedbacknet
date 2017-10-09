90f8052852ed1f0f6ab3b9de7b1dff2ab82d611f
John Emmons <emmons.john@gmail.com>
2017-10-09 01:23:16 -0700

    made the training and testing scripts more efficient

--- scripts/jemmons_train.sh
+++ scripts/jemmons_train.sh
@@ -8,10 +8,10 @@ fi
 export CUDA_VISIBLE_DEVICES=$1
 mkdir -p $2
 
-./vfeedbacknet_train /dev/shm/20bn-datasets/jester-v1-labels.csv \
-                     /dev/shm/20bn-datasets/jester-v1-validation.csv \
-                     /dev/shm/20bn-datasets/jester-v1-train.csv \
-                     /dev/shm/20bn-datasets/20bn-jester-v1 \
-                     $2 \
-                     $2/training_log.csv
+python3 -u vfeedbacknet_train /dev/shm/20bn-datasets/jester-v1-labels.csv \
+        /dev/shm/20bn-datasets/jester-v1-validation.csv \
+        /dev/shm/20bn-datasets/jester-v1-train.csv \
+        /dev/shm/20bn-datasets/20bn-jester-v1 \
+        $2 \
+        $2/training_log.csv
 
