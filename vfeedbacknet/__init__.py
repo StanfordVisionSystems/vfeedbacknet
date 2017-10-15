d23cb750e1b975a8fbfea069d398d7d83caa205f
John Emmons <emmons.john@gmail.com>
2017-10-15 16:05:44 -0700

    update models available for training

--- vfeedbacknet/__init__.py
+++ vfeedbacknet/__init__.py
@@ -1,5 +1,5 @@
 from vfeedbacknet.simple_model import simple_model
 from vfeedbacknet.nofeedbacknet_resnet import nofeedbacknet_resnet
-#from vfeedbacknet.vfeedbacknet_resnet import vfeedbacknet_resnet
+from vfeedbacknet.vfeedbacknet_resnet import vfeedbacknet_resnet
 #from vfeedbacknet.vfeedbacknet_vgg import vfeedbacknet_vgg
 
