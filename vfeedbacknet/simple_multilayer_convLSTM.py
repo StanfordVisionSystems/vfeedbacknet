0bb2607779bfe778f6047a2fff5d4c424a4001bc
John Emmons <emmons.john@gmail.com>
2017-10-17 18:59:23 -0700

    removed simple_model, added simple_multilayer_convLSTM

--- vfeedbacknet/simple_multilayer_convLSTM.py
+++ vfeedbacknet/simple_multilayer_convLSTM.py
@@ -10,7 +10,7 @@ from vfeedbacknet.convLSTM import ConvLSTMCell # https://github.com/StanfordVisi
 #logging.basicConfig(level=logging.INFO)
 logging.basicConfig(level=logging.DEBUG)
 
-LEAKINESS = 0.1
+LEAKINESS = 0.0
 
 class logger:
     count = {}
@@ -48,21 +48,31 @@ def simple_multilayer_convLSTM(video_length, video_width, video_height, num_labe
     
     # BEGIN CNN ################################################################
     logging.debug('---------- BEGIN CNN DEFINITION ----------')
-    with tf.device("/device:GPU:0"):
-        outputs = input_frames
+    outputs = input_frames
     
-        # layer 0 ##############################################################
-        conv_b = new_bias(16)
-        conv_w = new_conv2dweight(10, 10, 1, 16)
+    # layer 0 ##############################################################
+    with tf.device('/gpu:0'):
+        conv_b = new_bias(64)
+        conv_w = new_conv2dweight(7, 7, 1, 64)
         outputs = [ conv2d(output, conv_w, conv_b) for output in outputs ]
         logger.log('conv_output', outputs)
         
         outputs = [ max_pool(output) for output in outputs ]
         logger.log('maxpool_output', outputs)
         
+    # layer 1 ##############################################################
+    with tf.device('/gpu:0'):
+        conv_b = new_bias(64)
+        conv_w = new_conv2dweight(3, 3, 64, 64)
+        outputs = [ conv2d(output, conv_w, conv_b) for output in outputs ]
+        logger.log('conv_output', outputs)
+        
+        outputs = [ max_pool(output) for output in outputs ]
+        logger.log('maxpool_output', outputs)
+
     # convLSTM 1 (parts need to run on CPU) ####################################
     with tf.variable_scope('convlstm1'):
-        num_filters = 16 # convLSTM internal fitlers
+        num_filters = 16 #convLSTM internal fitlers
         h, w = int(outputs[0].shape[1]), int(outputs[0].shape[2])
         cell = ConvLSTMCell([h, w], num_filters, [3, 3], reuse=False)
         outputs, state = tf.nn.dynamic_rnn(
@@ -116,50 +126,54 @@ def simple_multilayer_convLSTM(video_length, video_width, video_height, num_labe
         outputs = tf.unstack(outputs, axis=1)
         logger.log('convLSTM_output', outputs)
         
-    #     # outputs = [tf.reduce_mean(output, axis=[1,2]) for output in outputs]
-    #     # logger.log('avepool_output', outputs)
-    
-    with tf.device("/device:CPU:0"):
-        h, w = int(outputs[0].shape[1]), int(outputs[0].shape[2])
-        outputs = [ tf.reshape(output, [-1, h*w*num_filters]) for output in outputs ]
+    # outputs = [tf.reduce_mean(output, axis=[1,2]) for output in outputs]
+    # logger.log('avepool_output', outputs)
+
+    with tf.device('/cpu:0'):
+        h, w, n = int(outputs[0].shape[1]), int(outputs[0].shape[2]), int(outputs[0].shape[3])
+        # h, w, n = 1, 1, num_filters
+        outputs = [ tf.reshape(output, [-1, h*w*n]) for output in outputs ]
         logger.log('flatten_output', outputs)
         
         b_fc = new_bias(num_labels)
-        w_fc = tf.Variable( tf.truncated_normal([h*w*num_filters, num_labels], stddev=0.1, ) )
+        w_fc = tf.Variable( tf.truncated_normal([h*w*n, num_labels], stddev=0.1, ) )
         outputs = [ tf.matmul(output, w_fc) + b_fc for output in outputs ]
-
         logger.log('fc_output', outputs)
     
-        final_outputs = outputs
+    final_outputs = outputs
 
     # END CNN ##################################################################
     logging.debug('---------- END CNN DEFINITION ----------')
 
     # ACCURACY AND LOSS ########################################################
-    with tf.device("/device:CPU:0"):
-        predictions = tf.stack([ tf.nn.softmax(logits=output) for output in final_outputs ], axis=1)
-        logging.debug('predictions: {}'.format(predictions.shape))
-        
-        cross_entropies = [ tf.nn.softmax_cross_entropy_with_logits(labels=output_placeholder, logits=output) for output in final_outputs ]
-        logging.debug('cross_entropies: {}x{}'.format(len(cross_entropies), cross_entropies[0].shape))
+    predictions = tf.stack([ tf.nn.softmax(logits=output) for output in final_outputs ], axis=1)
+    logging.debug('predictions: {}'.format(predictions.shape))
+    
+    cross_entropies = [ tf.nn.softmax_cross_entropy_with_logits(labels=output_placeholder, logits=output) for output in final_outputs ]
+    logging.debug('cross_entropies: {}x{}'.format(len(cross_entropies), cross_entropies[0].shape))
+    
+    # only use the final loss to update weights
+    #cross_entropies_truncated = [ tf.where(i > 0, zeros, cross_entropies[i]) for i in range(video_length) ]
+    cross_entropies_truncated = [ tf.where(i > input_length, zeros, cross_entropies[i]) for i in range(video_length) ]
 
-        # only use the final loss to update weights
-        cross_entropies_truncated = [ tf.where(i > input_length, zeros, cross_entropies[i]) for i in range(video_length) ]
-        #cross_entropies_truncated = [ tf.where(i < input_length, zeros, cross_entropies_truncated[i]) for i in range(video_length) ]
-        
-        losses = tf.stack(cross_entropies_truncated, axis=1, name='loss')
-        logging.debug('losses: {}'.format(losses.shape))
-
-        # logging.debug('intermediate_loss: {}'.format(tf.stack(cross_entropies_truncated).shape))
-        # logging.debug('divide length: {}'.format((tf.stack(cross_entropies_truncated)/tf.to_float(input_length)).shape))
-        
-        total_loss = tf.reduce_sum(tf.reduce_sum(tf.stack(cross_entropies_truncated)) / tf.to_float(input_length), name='total_loss')
-        logging.debug('total_loss: {}'.format(total_loss.shape))
+    # boost the loss on the last output by 10x
+    last_cross_entropy  = [ tf.where(i < input_length, zeros, cross_entropies_truncated[i]) for i in range(video_length) ]
+    cross_entropies_truncated = [ cross_entropies_truncated[i] + 10*last_cross_entropy[i] for i in range(video_length) ]
+    
+    losses = tf.stack(cross_entropies_truncated, axis=1, name='loss')
+    logging.debug('losses: {}'.format(losses.shape))
+    
+    # logging.debug('intermediate_loss: {}'.format(tf.stack(cross_entropies_truncated).shape))
+    # logging.debug('divide length: {}'.format((tf.stack(cross_entropies_truncated)/tf.to_float(input_length)).shape))
+    
+    total_loss = tf.reduce_sum(tf.reduce_sum(tf.stack(cross_entropies_truncated)) / tf.to_float(input_length), name='total_loss')
+    logging.debug('total_loss: {}'.format(total_loss.shape))
         
     return losses, total_loss, predictions
     
 def conv2d(x, w, b):
-    output = leaky_relu( batch_norm(tf.nn.conv2d(x, w, strides=[1,1,1,1], padding='SAME') + b) , LEAKINESS)
+    output = leaky_relu( tf.nn.bias_add(tf.nn.conv2d(x, w, strides=[1,1,1,1], padding='SAME'), b), LEAKINESS)
+    output = batch_norm(output)
     return output
 
 def leaky_relu(x, leakiness=0.0):
