452a7f91eb9cd320c3f51a422c84260b69c0ee60
John Emmons <emmons.john@gmail.com>
2017-10-25 22:25:29 -0700

    add checking to make sure old logs are not deleted

--- scripts/jemmons_train_ucf101.sh
+++ scripts/jemmons_train_ucf101.sh
@@ -5,6 +5,16 @@ if [[ -z $1 || -z $2 || -z $3 ]]; then
     exit 0
 fi
 
+EXTRA_ARGS=${*:4}
+if [[ -d $3 ]]; then
+    if [[ "$4" != "FORCE" ]]; then
+        echo 'dir already exists! please specify FORCE as 4th arg if you want to append to existing log'
+        exit 0
+    fi
+    echo 'forcing continued execution'
+    EXTRA_ARGS=${*:5}
+fi
+
 export CUDA_VISIBLE_DEVICES=$1
 mkdir -p $3
 
@@ -16,4 +26,4 @@ python3 -u vfeedbacknet_train $UCF101_ROOT/classInd.txt \
         $UCF101_ROOT/ \
         $2 \
         $3 \
-        $3/training_log.csv --ucf101 ${*:4} 2>&1 | tee -a $3/training_log.log 
+        $3/training_log.csv --ucf101 $EXTRA_ARGS 2>&1 | tee -a $3/training_log.log 
