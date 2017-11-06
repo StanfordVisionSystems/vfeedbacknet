15549cdc23670c11db89b85b4e8fd697a93a0ebc
John Emmons <emmons.john@gmail.com>
2017-11-05 23:03:59 -0800

    added new code

---
+++ scripts/jemmons_competition_20bn-jester.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [[ -z $1 || -z $2 ]]; then
+    echo "usage: ./jemmons_test.sh <GPU_NUM> <CHKT_PREFIX>"
+    exit 0
+fi
+
+export CUDA_VISIBLE_DEVICES=$1
+
+TWENTYBN_ROOT=/mnt/scratch/jemmons/20bn-jester
+
+python -u -B $DIR/vfeedbacknet_test $TWENTYBN_ROOT/jester-v1-labels.csv \
+                                 $TWENTYBN_ROOT/jester-v1-test.csv \
+                                 $TWENTYBN_ROOT/20bn-jester-v1 \
+                                 $2 --twentybn ${*:3}
